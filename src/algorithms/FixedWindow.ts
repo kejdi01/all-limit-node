29c6d2c40fff460da5547843015aa1bdf9e3634f
kejdimulla <kejdimulla@gmail.com>
2026-03-06 14:26:12 +0100

    refactor: refactored FixedWindow to use types and set state everytime if allowed or not

--- src/algorithms/FixedWindow.ts
+++ src/algorithms/FixedWindow.ts
@@ -7,33 +7,54 @@ export type FixedWindowState = {
   expiresAt: number;
 };
 
+type FixedWindowOptions = {
+  limit?: number;
+  window?: number;
+};
+
+type ResolvedFixedWindowOptions = {
+  limit: number;
+  window: number;
+};
+
 export class FixedWindow implements AlgorithmAdapter {
+  private options: ResolvedFixedWindowOptions;
   constructor(
     private storage: StorageAdapter<FixedWindowState>,
-    private limit: number,
-    private window: number,
-  ) {}
+    options: FixedWindowOptions = {},
+  ) {
+    this.options = {
+      limit: 10,
+      window: 60_000,
+      ...options,
+    };
+  }
 
   public async consume(key: string): Promise<RateLimiterResponse> {
     const now = Date.now();
-    let userData = await this.storage.get(key);
+    let windowState: FixedWindowState = (await this.storage.get(key)) || {
+      count: 0,
+      expiresAt: now + this.options.window,
+    };
 
-    if (!userData) {
-      userData = { count: 0, expiresAt: now + this.window };
+    if (now > windowState.expiresAt) {
+      windowState.count = 0;
+      windowState.expiresAt = now + this.options.window;
     }
 
-    const allowed = userData.count < this.limit;
+    const allowed = windowState.count < this.options.limit;
 
     if (allowed) {
-      userData.count++;
-      const remainingTTL = userData.expiresAt - now;
-      await this.storage.set(key, userData, remainingTTL);
+      windowState.count++;
     }
+    const remainingTTL = windowState.expiresAt - now;
+
+    await this.storage.set(key, windowState, remainingTTL);
 
     return {
       allowed,
-      resetAt: userData.expiresAt,
-      remaining: Math.max(0, this.limit - userData.count),
+      resetAt: windowState.expiresAt,
+      remaining: Math.max(0, this.options.limit - windowState.count),
     };
   }
 }
