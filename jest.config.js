c6bfc37bca7dac29a6151c065d1c15e415090c83
kejdimulla <kejdimulla@gmail.com>
2026-03-09 22:28:17 +0100

    chore: configure jest scripts for tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-const { createDefaultPreset } = require("ts-jest");
+import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
+/** @type {import('jest').Config} */
 export default {
   testEnvironment: "node",
   transform: {
